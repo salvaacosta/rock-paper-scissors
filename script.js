dcd1c3dec854d6dc659650df6621f8d02ebc6594
salvaacosta <sal.ab.lp@gmail.com>
2023-09-11 11:14:59 -0300

    Create loop for 5 games, create points system

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
-let userChoice = prompt("Choose Rock, Paper or Scissors");
-userChoice = userChoice.toLowerCase();
+function getUserChoices(){
+    let userChoice = prompt("Choose Rock, Paper or Scissors");
+    userChoice = userChoice.toLowerCase();
+    return userChoice;
+}
 
 
 function getComputerChoice(){
-    let options = ["paper","rock","scissors"];
-    let i = Math.floor(Math.random() * 3);
-    let choose = options[i];
-    return choose;
-}
+        let options = ["paper","rock","scissors"];
+        let i = Math.floor(Math.random() * 3);
+        let choose = options[i];
+        return choose;
+    }
+ 
+    let userPoints = 0;
+    let computerPoints = 0;
 
-let computerChoice = getComputerChoice();
+    function addUserPoints() {
+        point = userPoints + 1;
+        userPoints = point;
+    }
 
-function gamePlay() {
+    function addComputerPoints() {
+        point = computerPoints + 1;
+        computerPoints = point;
+    }
+
+
+for (let i = 0; i < 5; i++) {
+    
+    userChoice = getUserChoices();
+    let computerChoice = getComputerChoice();   
+
     if (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors"){
         console.log("Wrong option");
     }
 
     else if ((userChoice == "rock") && (computerChoice == "scissors")) {
-        console.log(`Computer choice: ${computerChoice}. You WON!!`)
+        console.log(`Computer choice: ${computerChoice}. You WON!!`);
+        addUserPoints();
+
     }
 
     else if ((userChoice == "paper") && (computerChoice == "rock")) {
-        console.log(`Computer choice: ${computerChoice}. You WON!!`)
+        console.log(`Computer choice: ${computerChoice}. You WON!!`);
+        addUserPoints();
     }
 
     else if ((userChoice == "scissors") && (computerChoice == "paper")) {
-        console.log(`Computer choice: ${computerChoice}. You WON!!`)
+        console.log(`Computer choice: ${computerChoice}. You WON!!`);
+        addUserPoints();
     }
 
     else if (userChoice == computerChoice){
@@ -33,8 +56,25 @@ function gamePlay() {
     }
     
     else {
-        console.log(`Computer choice: ${computerChoice}. You are a LOOSER!!`)
+        console.log(`Computer choice: ${computerChoice}. You LOOSE!!`);
+        addComputerPoints();
     }
+
 }
 
-gamePlay()
+if (userPoints == computerPoints) {
+    console.log(`User Points: ${userPoints}. Computer Points: ${computerPoints}. TIE`)
+}
+
+else if (userPoints > computerPoints) {
+    console.log(`User Points: ${userPoints}. Computer Points: ${computerPoints}. YOU ARE A WINNER!!`)
+
+}
+
+else { 
+    console.log(`User Points: ${userPoints}. Computer Points: ${computerPoints}. YOU ARE A LOOSER!!`)
+
+}
+
+    
+
